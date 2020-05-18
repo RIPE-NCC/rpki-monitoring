d0ea0def054cb18b2bd887484341979bcbe3ccab
Ties de Kock <ties@tiesdekock.nl>
2020-05-18 15:19:13 +0200

    Use graceful shutdown
      * Enable graceful shutdown explicitly. Why would you want this to be a
        default in 2020...

--- src/main/java/net/ripe/rpki/monitor/publishing/PublishedObjectStatus.java
+++ src/main/java/net/ripe/rpki/monitor/publishing/PublishedObjectStatus.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @Setter
 @RestController
 public class PublishedObjectStatus {
