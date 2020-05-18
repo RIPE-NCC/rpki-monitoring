6bd232803db48a9c6a4a8f5a99d7a2c7fdc71c6e
Ties de Kock <ties@tiesdekock.nl>
2020-05-18 10:49:28 +0200

    Remove direct call to core for listed objects

--- src/main/java/net/ripe/rpki/monitor/publishing/PublishedObjectStatus.java
+++ src/main/java/net/ripe/rpki/monitor/publishing/PublishedObjectStatus.java
@@ -1,8 +1,6 @@
 package net.ripe.rpki.monitor.publishing;
 
 import lombok.Setter;
-import net.ripe.rpki.monitor.service.core.CoreClient;
-import net.ripe.rpki.monitor.service.core.dto.PublishedObjectEntry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +11,7 @@ import java.util.List;
 @RestController
 public class PublishedObjectStatus {
     @Autowired
-    private CoreClient rpkiCoreClient;
-
-    @Autowired
     private PublishedObjectsSummary publishedObjectsSummary;
-
-    @GetMapping("/published-object-analysis")
-    public List<PublishedObjectEntry> publishedObjectAnalysis() {
-        return rpkiCoreClient.publishedObjects();
-    }
 
     @GetMapping("/published-object-diffs")
     public PublishedObjectsSummary.PublicationDiff publishedObjectDiffs() {
