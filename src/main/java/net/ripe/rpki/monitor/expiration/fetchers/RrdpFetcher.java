145814fe1d0c5713f88ed14ac6ce13d9ccdcbacb
Mikhail Puzanov <misha.puzanov@gmail.com>
2020-05-25 14:04:51 +0200

    Revert exception handling.

--- src/main/java/net/ripe/rpki/monitor/expiration/fetchers/RrdpFetcher.java
+++ src/main/java/net/ripe/rpki/monitor/expiration/fetchers/RrdpFetcher.java
@@ -1,6 +1,5 @@
 package net.ripe.rpki.monitor.expiration.fetchers;
 
-import com.google.common.hash.Hashing;
 import lombok.extern.slf4j.Slf4j;
 import net.ripe.rpki.monitor.util.Sha256;
 import net.ripe.rpki.monitor.util.XML;
@@ -14,11 +13,13 @@ import org.springframework.web.client.RestTemplate;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.Base64;
-import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -40,7 +41,7 @@ public class RrdpFetcher implements RepoFetcher {
         this.restTemplate = restTemplate;
     }
 
-    public Map<String, byte[]> fetchObjects() throws FetcherException {
+    public Map<String, byte[]> fetchObjects() throws FetcherException, SnapshotNotModifiedException {
         try {
             final DocumentBuilder documentBuilder = XML.newDocumentBuilder();
 
@@ -82,8 +83,10 @@ public class RrdpFetcher implements RepoFetcher {
                     final byte[] decoded = decoder.decode(item.getTextContent());
 
                     return ImmutablePair.of(objectUri, decoded);
-                }).collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
-        } catch (Exception e) {
+                })
+                .collect(Collectors.toMap(Pair::getLeft, Pair::getRight));
+
+        } catch (ParserConfigurationException | SAXException | IOException | SnapshotWrongHashException e) {
             throw new FetcherException(e);
         }
     }
