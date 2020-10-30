05ce0f4d493f9a1e548a9c55dc651e89c84f61f7
Mikhail Puzanov <misha.puzanov@gmail.com>
2020-10-30 15:03:59 +0100

    Add some initial code for the rsync repository comparison.

--- src/main/java/net/ripe/rpki/monitor/compare/RsyncComparisonService.java
+++ src/main/java/net/ripe/rpki/monitor/compare/RsyncComparisonService.java
@@ -1,58 +1,50 @@
 package net.ripe.rpki.monitor.compare;
 
+import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
-import net.ripe.rpki.monitor.expiration.fetchers.FetcherException;
-import net.ripe.rpki.monitor.expiration.fetchers.RsyncFetcher;
+import io.micrometer.core.instrument.MeterRegistry;
+import net.ripe.rpki.monitor.publishing.dto.FileEntry;
 import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
+
 
 /**
  * Fetches two rsync repositories, compares them and reports the result.
  */
 @Slf4j
-public class RsyncComparison {
+@Service
+public class RsyncComparisonService {
 
-    private RsyncFetcher fetcher1;
-    private RsyncFetcher fetcher2;
+    private final MeterRegistry registry;
+    private final String rsyncPremise;
+    private final String rsyncAws;
 
-    public RsyncComparison(String url1, String url2) {
-        fetcher1 = new RsyncFetcher();
-        fetcher1.setRsyncUrl(url1);
-        fetcher2 = new RsyncFetcher();
-        fetcher2.setRsyncUrl(url2);
+    @Autowired
+    public RsyncComparisonService(MeterRegistry registry,
+                                  @Value("${rsync.url}") String rsyncPremise,
+                                  @Value("${rsync.aws-url}") String rsyncAws) {
+        this.registry = registry;
+        this.rsyncPremise = rsyncPremise;
+        this.rsyncAws = rsyncAws;
     }
 
-    void compare() {
-        Optional<Pair<Map<String, byte[]>, Map<String, byte[]>>> maps = fetchBoth();
+
+    void updateRsyncComparisonMetrics() {
+        final RsyncComparison comparison = new RsyncComparison(rsyncPremise, rsyncAws);
+        Optional<Pair<Map<String, byte[]>, Map<String, byte[]>>> maps = comparison.fetchBoth();
         if (maps.isEmpty()) {
             log.info("Could not fetch at least one of the repositories, bailing out");
         } else {
             Map<String, byte[]> m1 = maps.get().getLeft();
             Map<String, byte[]> m2 = maps.get().getRight();
 
-        }
-    }
-    
-    Optional<Pair<Map<String, byte[]>, Map<String, byte[]>>> fetchBoth() {
-        try {
-            final CompletableFuture<Map<String, byte[]>> f1 = CompletableFuture.supplyAsync(() -> fetchRepo(fetcher1));
-            final CompletableFuture<Map<String, byte[]>> f2 = CompletableFuture.supplyAsync(() -> fetchRepo(fetcher2));
-            return Optional.of(Pair.of(f1.get(), f2.get()));
-        } catch (Exception e) {
-            log.error("Fetch error: ", e);
-            return Optional.empty();
-        }
-    }
 
-    private Map<String, byte[]> fetchRepo(RsyncFetcher fetcher) {
-        try {
-            return fetcher.fetchObjects();
-        } catch (FetcherException e) {
-            throw new RuntimeException(e);
+
         }
     }
-
 }
