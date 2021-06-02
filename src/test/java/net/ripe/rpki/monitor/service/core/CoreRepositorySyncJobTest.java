e10196cf25496fc399ac81a3b3a2e6f0d21fb290
Bart Bakker <bbakker@ripe.net>
2021-06-02 15:54:10 +0200

    Add job that syncs core repository objects

---
+++ src/test/java/net/ripe/rpki/monitor/service/core/CoreRepositorySyncJobTest.java
@@ -0,0 +1,32 @@
+package net.ripe.rpki.monitor.service.core;
+
+import net.ripe.rpki.monitor.repositories.RepositoriesState;
+import net.ripe.rpki.monitor.service.core.dto.PublishedObjectEntry;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Test;
+import org.quartz.JobExecutionContext;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class CoreRepositorySyncJobTest {
+    private final CoreClient coreClientStub = mock(CoreClient.class);
+    private final RepositoriesState state = RepositoriesState.init(List.of(Pair.of("core", "https://ba-apps.ripe.net/certification/")));
+
+    @Test
+    public void test_update_state_of_core() throws Exception {
+        var object = PublishedObjectEntry.builder()
+                .sha256("51f83dd6a174889f29038686aca85c44b64b87a4f1ab702c5b531b3c5687fc0e")
+                .uri("rsync://rpki.ripe.net/repository/DEFAULT/xyz.cer")
+                .build();
+        when(coreClientStub.publishedObjects()).thenReturn(List.of(object));
+
+        var subject = new CoreRepositorySyncJob(state, coreClientStub);
+        subject.execute(mock(JobExecutionContext.class, RETURNS_DEEP_STUBS));
+
+        assertThat(state.getTrackerByTag("core").get().size(Instant.now())).isEqualTo(1);
+    }
+}
