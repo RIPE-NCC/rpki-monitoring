cb37b38d54730cf8d1e3c0f0b53eba5e6a9d40c4
Bart Bakker <bbakker@ripe.net>
2022-02-28 20:07:56 +0100

    Extract http client implementation to class

--- src/main/java/net/ripe/rpki/monitor/expiration/fetchers/RrdpFetcher.java
+++ src/main/java/net/ripe/rpki/monitor/expiration/fetchers/RrdpFetcher.java
@@ -5,26 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import net.ripe.rpki.monitor.MonitorProperties;
 import net.ripe.rpki.monitor.RrdpConfig;
 import net.ripe.rpki.monitor.publishing.dto.RpkiObject;
+import net.ripe.rpki.monitor.util.Http;
 import net.ripe.rpki.monitor.util.Sha256;
 import net.ripe.rpki.monitor.util.XML;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.hc.client5.http.SystemDefaultDnsResolver;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
-import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.http.URIScheme;
-import org.apache.hc.core5.http.config.RegistryBuilder;
-import org.apache.hc.core5.pool.PoolConcurrencyPolicy;
-import org.apache.hc.core5.pool.PoolReusePolicy;
-import org.apache.hc.core5.util.TimeValue;
-import org.apache.hc.core5.util.Timeout;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -34,8 +19,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Base64;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -47,19 +30,14 @@ import static com.google.common.base.Verify.verifyNotNull;
 @Getter
 public class RrdpFetcher implements RepoFetcher {
 
-    private final MonitorProperties properties;
     private final RrdpConfig.RrdpRepositoryConfig config;
-    private final PoolingHttpClientConnectionManager connectionManager;
+    private final Http http;
 
     private String lastSnapshotUrl;
 
-    public RrdpFetcher(
-            RrdpConfig.RrdpRepositoryConfig config,
-            MonitorProperties properties
-    ) {
-        this.properties = properties;
+    public RrdpFetcher(RrdpConfig.RrdpRepositoryConfig config, MonitorProperties properties) {
         this.config = config;
-        this.connectionManager = createConnectionManager(config);
+        this.http = new Http(String.format("rpki-monitor %s", properties.getVersion()), config.getConnectTo());
 
         log.info("RrdpFetcher({}, {}, {})", config.getName(), config.getNotificationUrl(), config.getOverrideHostname());
     }
@@ -73,7 +51,7 @@ public class RrdpFetcher implements RepoFetcher {
         try {
             final DocumentBuilder documentBuilder = XML.newDocumentBuilder();
 
-            final String notificationXml = fetch(config.getNotificationUrl());
+            final String notificationXml = http.fetch(config.getNotificationUrl());
             verifyNotNull(notificationXml);
             final Document notificationXmlDoc = documentBuilder.parse(new ByteArrayInputStream(notificationXml.getBytes()));
 
@@ -90,7 +68,7 @@ public class RrdpFetcher implements RepoFetcher {
 
             log.info("loading RRDP snapshot from {}", snapshotUrl);
 
-            final String snapshotXml = fetch(snapshotUrl);
+            final String snapshotXml = http.fetch(snapshotUrl);
             assert snapshotXml != null;
             final byte[] bytes = snapshotXml.getBytes();
 
@@ -117,55 +95,6 @@ public class RrdpFetcher implements RepoFetcher {
 
         } catch (ParserConfigurationException | SAXException | IOException | SnapshotWrongHashException e) {
             throw new FetcherException(e);
-        }
-    }
-
-    private String fetch(String url) throws IOException {
-        var requestConfig = RequestConfig.custom()
-                .setConnectTimeout(Timeout.ofSeconds(10))
-                .setRedirectsEnabled(false)
-                .build();
-        try (var client = httpClient(connectionManager)) {
-            var req = new HttpGet(url);
-            req.setConfig(requestConfig);
-            req.addHeader("User-Agent", String.format("rpki-monitor %s", properties.getVersion()));
-            return client.execute(req, new BasicHttpClientResponseHandler());
-        }
-    }
-
-    private static CloseableHttpClient httpClient(PoolingHttpClientConnectionManager connectionManager) {
-        return HttpClients.custom()
-                .setConnectionManager(connectionManager)
-                .setConnectionManagerShared(true)
-                .build();
-    }
-
-    private static PoolingHttpClientConnectionManager createConnectionManager(RrdpConfig.RrdpRepositoryConfig config) {
-        var resolver = new ConnectToResolver(config.getConnectTo());
-        var socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create()
-                .register(URIScheme.HTTP.id, PlainConnectionSocketFactory.getSocketFactory())
-                .register(URIScheme.HTTPS.id, SSLConnectionSocketFactory.getSocketFactory())
-                .build();
-        return new PoolingHttpClientConnectionManager(
-                socketFactoryRegistry,
-                PoolConcurrencyPolicy.STRICT,
-                PoolReusePolicy.LIFO,
-                TimeValue.ofSeconds(60),
-                null,
-                resolver,
-                null
-        );
-    }
-    static class ConnectToResolver extends SystemDefaultDnsResolver {
-        private final Map<String, String> connectTo;
-
-        public ConnectToResolver(Map<String, String> connectTo) {
-            this.connectTo = Map.copyOf(connectTo);
-        }
-
-        @Override
-        public InetAddress[] resolve(String host) throws UnknownHostException {
-            return super.resolve(connectTo.getOrDefault(host, host));
         }
     }
 }
---
+++ src/main/java/net/ripe/rpki/monitor/util/Http.java
@@ -0,0 +1,96 @@
+package net.ripe.rpki.monitor.util;
+
+import org.apache.hc.client5.http.SystemDefaultDnsResolver;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.URIScheme;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.pool.PoolConcurrencyPolicy;
+import org.apache.hc.core5.pool.PoolReusePolicy;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+
+/**
+ * HTTP client wrapper that fetches data.
+ * <p>
+ * Using the `connectTo` map requests can be sent to a different host. The origin host is used for TLS (SNI and
+ * certificate validation) and in the Host header.
+ */
+public class Http {
+    private final String userAgent;
+    private final HttpClientConnectionManager connectionManager;
+
+    public Http(String userAgent, Map<String, String> connectTo) {
+        this.userAgent = userAgent;
+        this.connectionManager = createConnectionManager(connectTo);
+    }
+
+    public String fetch(String url) throws IOException {
+        var requestConfig = RequestConfig.custom()
+                .setConnectTimeout(Timeout.ofSeconds(10))
+                .setRedirectsEnabled(false)
+                .build();
+        try (var client = createHttpClient()) {
+            var req = new HttpGet(url);
+            req.setConfig(requestConfig);
+            req.addHeader("User-Agent", userAgent);
+            return client.execute(req, new BasicHttpClientResponseHandler());
+        }
+    }
+
+    private CloseableHttpClient createHttpClient() {
+        return HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setConnectionManagerShared(true)
+                .build();
+    }
+
+    private static PoolingHttpClientConnectionManager createConnectionManager(Map<String, String> connectTo) {
+        var resolver = new ConnectToResolver(connectTo);
+        var socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register(URIScheme.HTTP.id, PlainConnectionSocketFactory.getSocketFactory())
+                .register(URIScheme.HTTPS.id, SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+        return new PoolingHttpClientConnectionManager(
+                socketFactoryRegistry,
+                PoolConcurrencyPolicy.STRICT,
+                PoolReusePolicy.LIFO,
+                TimeValue.ofSeconds(60),
+                null,
+                resolver,
+                null
+        );
+    }
+}
+
+/**
+ * DNS resolver that substitutes hosts when specified by the `connectTo` attribute.
+ *
+ * When a host has an entry in `connectTo`, the mapped host is resolved instead. I.e. given an entry `HOST1: HOST2`
+ * in `connectTo` will resolve the address of `HOST2` when attempting to resolve `HOST1`.
+ */
+class ConnectToResolver extends SystemDefaultDnsResolver {
+    private final Map<String, String> connectTo;
+
+    public ConnectToResolver(Map<String, String> connectTo) {
+        this.connectTo = Map.copyOf(connectTo);
+    }
+
+    @Override
+    public InetAddress[] resolve(String host) throws UnknownHostException {
+        return super.resolve(connectTo.getOrDefault(host, host));
+    }
+}
