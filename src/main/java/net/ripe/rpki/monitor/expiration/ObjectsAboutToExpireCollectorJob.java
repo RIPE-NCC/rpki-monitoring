6099c8d4b06f4a50c46726fa1911805b5ea18b9c
Ties de Kock <ties@tiesdekock.nl>
2023-02-22 15:28:10 +0100

    Reduce SonarQube warnings

--- src/main/java/net/ripe/rpki/monitor/expiration/ObjectsAboutToExpireCollectorJob.java
+++ src/main/java/net/ripe/rpki/monitor/expiration/ObjectsAboutToExpireCollectorJob.java
@@ -1,8 +1,6 @@
 package net.ripe.rpki.monitor.expiration;
 
 import lombok.SneakyThrows;
-import net.ripe.rpki.monitor.expiration.fetchers.FetcherException;
-import net.ripe.rpki.monitor.expiration.fetchers.SnapshotStructureException;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -16,18 +14,15 @@ public class ObjectsAboutToExpireCollectorJob extends QuartzJobBean {
         this.collectors = collectors;
     }
 
-    private static void safeRunCollector(ObjectAndDateCollector collector) {
-        try {
-            collector.run();
-        } catch (FetcherException | SnapshotStructureException e) {
-            throw new RuntimeException(e);
-        }
+    @SneakyThrows
+    private static void runCollector(ObjectAndDateCollector collector) {
+        collector.run();
     }
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         try {
-            collectors.parallelStream().forEach(ObjectsAboutToExpireCollectorJob::safeRunCollector);
+            collectors.parallelStream().forEach(ObjectsAboutToExpireCollectorJob::runCollector);
         } catch (Exception e) {
             throw new JobExecutionException(e);
         }
