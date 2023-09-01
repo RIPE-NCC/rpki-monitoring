ae56bb453019da410de341f610e0de0ba632b514
Ties de Kock <ties@tiesdekock.nl>
2023-09-01 14:50:12 +0200

    Test the metric tag

--- src/test/java/net/ripe/rpki/monitor/expiration/RrdpConfigTest.java
+++ src/test/java/net/ripe/rpki/monitor/expiration/RrdpConfigTest.java
@@ -15,14 +15,20 @@ public class RrdpConfigTest {
         final var config = new RrdpConfig.RrdpRepositoryConfig("test-config", baseUrl + "notification.xml", null, Map.of(), Duration.ZERO);
 
         then(config.overrideHostname(baseUrl + "12343/1/delta.xml")).isEqualTo(baseUrl + "12343/1/delta.xml");
+        then(config.metricUrlTag())
+                .doesNotContain("@");
     }
 
     @Test
     public void testOverrideHostname_different_host() {
-        final var baseUrl = "https://rrdp.example.org/";
+        final var baseUrl = "https://rrdp.other.host.example.org/";
         final var config = new RrdpConfig.RrdpRepositoryConfig("test-config", baseUrl + "notification.xml", "rrdp.example.org", Map.of(), Duration.ZERO);
 
-        then(config.overrideHostname("https://rrdp.other.host.example.org/12343/1/delta.xml")).isEqualTo(baseUrl + "12343/1/delta.xml");
+        then(config.overrideHostname("https://rrdp.other.host.example.org/12343/1/delta.xml")).isEqualTo("https://rrdp.example.org/12343/1/delta.xml");
+        then(config.metricUrlTag())
+                .contains("https://rrdp.other.host.example.org/notification.xml")
+                .contains("@rrdp.example.org")
+                .doesNotContain("=");
     }
 
     @Test
@@ -32,4 +38,14 @@ public class RrdpConfigTest {
         then(config.overrideHostname(brokenUrl)).isEqualTo(brokenUrl);
         then(config.overrideHostname(null)).isNull();
     }
+
+    @Test
+    public void testConnectTo() {
+        final var baseUrl = "https://rrdp.example.org/";
+        final var config = new RrdpConfig.RrdpRepositoryConfig("test-config", baseUrl + "notification.xml", null, Map.of("rrdp.example.org", "rrdp.example.org.cnd.example.org"), Duration.ZERO);
+
+        then(config.metricUrlTag())
+                .contains("@rrdp.example.org=rrdp.example.org.cnd.example.org");
+    }
+
 }
