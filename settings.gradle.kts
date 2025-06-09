f2a3230c293b388b3174ac7b1c15b334093b964e
service_account_group_1283_e7ab0860da192e015b1cb71ee336923f <service_account_group_1283_e7ab0860da192e015b1cb71ee336923f@noreply.gitlab.ripe.net>
2025-06-09 09:01:33 +0000

    chore(deps): update plugin org.gradle.toolchains.foojay-resolver-convention to v1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.name = "rpki-monitoring"
 
 plugins {
     // Toolchain resolver allows auto-download of JDK 21 which is not in container
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.10.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version("1.0.0")
 }
