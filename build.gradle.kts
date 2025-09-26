84685ee3c594a9604385b9346571b84c723b214a
service_account_group_1283_e7ab0860da192e015b1cb71ee336923f <service_account_group_1283_e7ab0860da192e015b1cb71ee336923f@noreply.gitlab.ripe.net>
2025-09-26 08:45:40 +0000

    fix(deps): update dependency org.assertj:assertj-core to v3.27.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude("org.junit.vintage", "junit-vintage-engine")
     }
-    testImplementation("org.assertj:assertj-core:3.27.5")
+    testImplementation("org.assertj:assertj-core:3.27.6")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 }
 
