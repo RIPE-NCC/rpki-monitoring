6a9f008d53d0a4eb851513a9928e1a932e6b6da8
service_account_group_1283_e7ab0860da192e015b1cb71ee336923f <service_account_group_1283_e7ab0860da192e015b1cb71ee336923f@noreply.gitlab.ripe.net>
2026-03-19 08:18:35 +0000

    chore(deps): update plugin com.gorylenko.gradle-git-properties to v2.5.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     java
     id("io.freefair.lombok") version "9.1.0"
     id("org.springframework.boot") version "3.5.9"
-    id("com.gorylenko.gradle-git-properties") version "2.5.4"
+    id("com.gorylenko.gradle-git-properties") version "2.5.7"
     jacoco
     id("org.sonarqube") version "7.2.3.7755"
 
